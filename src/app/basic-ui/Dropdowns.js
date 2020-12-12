44bd8efc3b87ce786856294d2715d9b84701b986
taofik-adeniyi <bidemi64@gmail.com>
2020-12-12 09:37:28 +0100

    first commit and interface done

---
+++ src/app/basic-ui/Dropdowns.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+import { Dropdown, ButtonGroup, Pagination, Page } from 'react-bootstrap';
+
+
+let active = 2;
+    let items = [];
+    for (let number = 1; number <= 5; number++) {
+      items.push(
+        <Pagination.Item key={number} active={number === active}>
+          {number}
+        </Pagination.Item>,
+      );
+    }
+export class Dropdowns extends Component {
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <h3 className="page-title"> All Won Trivia </h3>
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              {/* <li className="breadcrumb-item"> */}
+                {/* <a href="!#" onClick={event => event.preventDefault()}> */}
+                  
+                {/* </a> */}
+                {/* </li> */}
+              <li className="breadcrumb-item active" aria-current="page"> Won Trivia</li>
+            </ol>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="col-lg-12 grid-margin stretch-card">
+            <Dropdown>
+              <Dropdown.Toggle variant="btn btn-outline-primary" id="dropdownMenuOutlineButton1">
+                Filter
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Header>50</Dropdown.Header>
+                <Dropdown.Item>100</Dropdown.Item>
+                <Dropdown.Item>150</Dropdown.Item>
+                <Dropdown.Item>200</Dropdown.Item>
+                <Dropdown.Item>250</Dropdown.Item>
+                <Dropdown.Item>300</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+        <div className="row">
+        <div className="col-lg-12 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">All Players</h4>
+                {/* <p className="card-description"> Add className <code>.table-striped</code> */}
+                {/* </p> */}
+                <div className="table-responsive">
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th> User </th>
+                        <th> First name </th>
+                        <th> Progress </th>
+                        <th> Amount </th>
+                        <th> Deadline </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face1.jpg")} alt="user icon" />
+                        </td>
+                        <td> Herman Beck </td>
+                        <td>New Here</td>
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face2.jpg")} alt="user icon" />
+                        </td>
+                        <td> Messsy Adam </td>
+                        <td>How are you</td>
+                        <td> $245.30 </td>
+                        <td> July 1, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face3.jpg")} alt="user icon" />
+                        </td>
+                        <td> John Richards </td>
+                        <td>Almost there</td>
+                        <td> $138.00 </td>
+                        <td> Apr 12, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face4.jpg")} alt="user icon" />
+                        </td>
+                        <td> Peter Meggik </td>
+                        <td>right here</td>
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face5.jpg")} alt="user icon" />
+                        </td>
+                        <td> Edward </td>
+                        <td>Now</td>
+                        <td> $ 160.25 </td>
+                        <td> May 03, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face6.jpg")} alt="user icon" />
+                        </td>
+                        <td> John Doe </td>
+                        <td>Knew</td>
+                        <td> $ 123.21 </td>
+                        <td> April 05, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td className="py-1">
+                          <img src={require("../../assets/images/faces/face7.jpg")} alt="user icon" />
+                        </td>
+                        <td> Henry Tom </td>
+                        <td>Dont know</td>
+                        <td> $ 150.00 </td>
+                        <td> June 16, 2015 </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+            <Pagination size="sm">{items}</Pagination>
+          </div>
+          <div>
+            <Pagination>
+              <Pagination.First />
+              <Pagination.Prev />
+              <Pagination.Item>{1}</Pagination.Item>
+              <Pagination.Ellipsis />
+
+              <Pagination.Item>{10}</Pagination.Item>
+              <Pagination.Item>{11}</Pagination.Item>
+              <Pagination.Item active>{12}</Pagination.Item>
+              <Pagination.Item>{13}</Pagination.Item>
+              <Pagination.Item disabled>{14}</Pagination.Item>
+
+              <Pagination.Ellipsis />
+              <Pagination.Item>{20}</Pagination.Item>
+              <Pagination.Next />
+              <Pagination.Last />
+            </Pagination>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default Dropdowns
---
+++ src/app/tables/BasicTable.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react'
+import { ProgressBar, Dropdown, Pagination } from 'react-bootstrap';
+
+
+
+let active = 2;
+    let items = [];
+    for (let number = 1; number <= 5; number++) {
+      items.push(
+        <Pagination.Item key={number} active={number === active}>
+          {number}
+        </Pagination.Item>,
+      );
+    }
+class BasicTable extends Component {
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <h3 className="page-title"> All Players </h3>
+          {/* <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item"><a href="!#" onClick={event => event.preventDefault()}>Tables</a></li>
+              <li className="breadcrumb-item active" aria-current="page">Basic tables</li>
+            </ol>
+          </nav> */}
+        </div>
+        <div className="row">
+          <div className="col-lg-12 grid-margin stretch-card">
+            <Dropdown>
+              <Dropdown.Toggle variant="btn btn-outline-primary" id="dropdownMenuOutlineButton1">
+                Filter
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Header>50</Dropdown.Header>
+                <Dropdown.Item>100</Dropdown.Item>
+                <Dropdown.Item>150</Dropdown.Item>
+                <Dropdown.Item>200</Dropdown.Item>
+                <Dropdown.Item>250</Dropdown.Item>
+                <Dropdown.Item>300</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+        <div className="row">    
+          <div className="col-lg-12 stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">
+                  List of All Active Users
+                </h4>
+                {/* <p className="card-description"> Add className <code>.table-&#123;color&#125;</code>
+                </p> */}
+                <div className="table-responsive">
+                  <table className="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th> # </th>
+                        <th> First name </th>
+                        <th> Product </th>
+                        <th> Amount </th>
+                        <th> Deadline </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className="table-info">
+                        <td> 1 </td>
+                        <td> Herman Beck </td>
+                        <td> Photoshop </td>
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+                      <tr className="table-warning">
+                        <td> 2 </td>
+                        <td> Messsy Adam </td>
+                        <td> Flash </td>
+                        <td> $245.30 </td>
+                        <td> July 1, 2015 </td>
+                      </tr>
+                      <tr className="table-danger">
+                        <td> 3 </td>
+                        <td> John Richards </td>
+                        <td> Premeire </td>
+                        <td> $138.00 </td>
+                        <td> Apr 12, 2015 </td>
+                      </tr>
+                      <tr className="table-success">
+                        <td> 4 </td>
+                        <td> Peter Meggik </td>
+                        <td> After effects </td>
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+                      <tr className="table-primary">
+                        <td> 5 </td>
+                        <td> Edward </td>
+                        <td> Illustrator </td>
+                        <td> $ 160.25 </td>
+                        <td> May 03, 2015 </td>
+                      </tr>
+                      <tr className="table-primary">
+                        <td> 6 </td>
+                        <td> Edward </td>
+                        <td> Illustrator </td>
+                        <td> $ 160.25 </td>
+                        <td> May 03, 2015 </td>
+                      </tr>
+                      <tr className="table-primary">
+                        <td> 7 </td>
+                        <td> Edward </td>
+                        <td> Illustrator </td>
+                        <td> $ 160.25 </td>
+                        <td> May 03, 2015 </td>
+                      </tr>
+                      <tr className="table-primary">
+                        <td> 8 </td>
+                        <td> Edward </td>
+                        <td> Illustrator </td>
+                        <td> $ 160.25 </td>
+                        <td> May 03, 2015 </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row grid-margin" style={{margin: "10px"}}>
+          <Pagination size="sm">{items}</Pagination>
+        </div>
+        <div className="row grid-margin">
+          <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Ellipsis />
+
+            <Pagination.Item>{10}</Pagination.Item>
+            <Pagination.Item>{11}</Pagination.Item>
+            <Pagination.Item active>{12}</Pagination.Item>
+            <Pagination.Item>{13}</Pagination.Item>
+            <Pagination.Item disabled>{14}</Pagination.Item>
+
+            <Pagination.Ellipsis />
+            <Pagination.Item>{20}</Pagination.Item>
+            <Pagination.Next />
+            <Pagination.Last />
+          </Pagination>
+        </div>
+      </div>
+      
+    )
+  }
+}
+
+export default BasicTable
