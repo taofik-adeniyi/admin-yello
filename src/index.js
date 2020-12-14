9e5e0d61c7edc411c841362087c6d1c953bc7740
taofik-adeniyi <bidemi64@gmail.com>
2020-12-14 14:38:46 +0100

    login working

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./i18n";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter basename="/yellostar">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 , document.getElementById('root'));
